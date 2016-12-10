efc20150469abcc5984957cdd7ac6e70135ea89c
Ryuichi Ueda <ryuichiueda@gmail.com>
2016-12-10 14:56:24 +0000

    Add clone procedure

--- test/travis_package_make.bash
+++ test/travis_package_make.bash
@@ -2,5 +2,10 @@
 
 #sync and make
 rsync -av ./ ~/catkin_ws/src/pimouse_run_corridor/
+
+#clone pimouse_ros
+cd ~/catkin_ws/src/
+git clone https://github.com/citueda/pimouse_ros.git
+
 cd ~/catkin_ws
 catkin_make
