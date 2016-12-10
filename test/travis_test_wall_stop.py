31f496287a63c9afd12c997cf3b186399edfedb1
Ryuichi Ueda <ryuichiueda@gmail.com>
2016-12-10 14:14:58 +0000

    Add a test script

--- test/travis_test_wall_stop.py
+++ test/travis_test_wall_stop.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 import unittest, rostest
 import rosnode, rospy
-import time
+import time, sys
 from std_msgs.msg import UInt16
+from std_srvs.srv import Trigger, TriggerResponse
 
 class WallStopTest(unittest.TestCase):
     def test_node_exist(self):
@@ -11,27 +12,27 @@ class WallStopTest(unittest.TestCase):
 
     def set_sensor_values(self,lf,ls,rs,rf):
         with open("/dev/rtlightsensor0","w") as f:
-            f.write("%d %d %d %d\n" % (lf,ls,rs,rf))
+            f.write("%d %d %d %d\n" % (rf,rs,ls,lf))
 
     def get_freqs(self):
         with open("/dev/rtmotor_raw_l0","r") as lf,\
-             open("/dev/rtmotor_raw_r0","r") as rf,
-            left = int(lf.readline().strip())
-            right = int(lf.readline().strip())
-
+             open("/dev/rtmotor_raw_r0","r") as rf:
+            left = int(lf.readline().rstrip())
+            right = int(rf.readline().rstrip())
         return left,right
 
     def test_io(self):
-        set_sensor_values(400,0,100,0)
-        time.sleep(0.1)
-        left, right = get_freqs()
+        self.set_sensor_values(400,100,100,0)
+        time.sleep(0.3)
+        left, right = self.get_freqs()
         self.assertTrue(left == 0 and right == 0,"can't stop")
 
-        set_sensor_values(400,0,0,99)
-        time.sleep(0.1)
-        left, right = get_freqs()
+        self.set_sensor_values(400,0,0,99)
+        time.sleep(0.3)
+        left, right = self.get_freqs()
         self.assertTrue(left != 0 and right != 0,"can't move again")
 
 if __name__ == '__main__':
+    time.sleep(3)
     rospy.init_node('travis_test_wall_stop')
     rostest.rosrun('pimouse_run_corridor','travis_test_wall_stop',WallStopTest)
