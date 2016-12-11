8b4863f93c36f9936859d100a283ed53922d0065
Ryuichi Ueda <ryuichiueda@gmail.com>
2016-12-11 06:29:23 +0000

    Add an error message

--- scripts/wall_stop.py
+++ scripts/wall_stop.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import rospy,copy
+import rospy
 from geometry_msgs.msg import Twist
 from std_srvs.srv import Trigger, TriggerResponse
 from pimouse_ros.msg import LightSensorValues
@@ -9,10 +9,10 @@ class WallTrace():
     def __init__(self):
         self.cmd_vel = rospy.Publisher('/cmd_vel',Twist,queue_size=1)
         rospy.Subscriber('/lightsensors', LightSensorValues, self.callback_lightsensors)
-        self.sensor_values = LightSensorValues()
+        self.sensors = LightSensorValues()
 
     def callback_lightsensors(self,messages):
-        self.sensor_values = messages
+        self.sensors = messages
 
     def decision(self,sensor_sum_all):
         linear = 0.0 if sensor_sum_all >= 500 else 0.2
@@ -23,16 +23,18 @@ class WallTrace():
         rate = rospy.Rate(10)
         d = Twist()
         while not rospy.is_shutdown():
-            d.linear.x, d.angular.z = self.decision(self.sensor_values.sum_all)
+            d.linear.x, d.angular.z = self.decision(self.sensors.sum_all)
             self.cmd_vel.publish(d)
             rate.sleep()
 
 if __name__ == '__main__':
     rospy.init_node('wall_trace')
-
     rospy.wait_for_service('/motor_on')
     rospy.wait_for_service('/motor_off')
+
     rospy.on_shutdown(rospy.ServiceProxy('/motor_off',Trigger).call)
-    rospy.ServiceProxy('/motor_on',Trigger).call()
+    if not rospy.ServiceProxy('/motor_on',Trigger).call().success:
+        rospy.logerr("motors are not empowered")
+        sys.exit(1)
 
     WallTrace().run()
