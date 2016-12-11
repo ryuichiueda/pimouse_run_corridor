ea0ec5dd25189fbc168b50b30b5a10f5b892ab1a
Ryuichi Ueda <ryuichiueda@gmail.com>
2016-12-11 06:10:58 +0000

    Reduce a line

--- scripts/wall_stop.py
+++ scripts/wall_stop.py
@@ -37,5 +37,4 @@ if __name__ == '__main__':
     rospy.on_shutdown(rospy.ServiceProxy('/motor_off',Trigger).call)
     rospy.ServiceProxy('/motor_on',Trigger).call()
 
-    w = WallTrace()
-    w.run()
+    WallTrace().run()
