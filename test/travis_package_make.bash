1aa3cfd14c17887bb8239b42ef00931dbeaaa1f6
Ryuichi Ueda <ryuichiueda@gmail.com>
2016-12-11 02:05:15 +0000

    Customize a script for this repo

--- test/travis_package_make.bash
+++ test/travis_package_make.bash
@@ -1,6 +1,11 @@
 #!/bin/bash -xve
 
-#sync and make
-rsync -av ./ ~/catkin_ws/src/pimouse_ros/
+#sync this repository to the workspace
+rsync -av ./ ~/catkin_ws/src/pimouse_run_corridor/
+
+#clone pimouse_ros
+cd ~/catkin_ws/src/
+git clone https://github.com/citueda/pimouse_ros.git
+
 cd ~/catkin_ws
 catkin_make
