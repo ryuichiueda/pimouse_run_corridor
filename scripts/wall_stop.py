c3ea1ea3b65f25d9ad77f288e1b4fb43952a038a
Ryuichi Ueda <ryuichiueda@gmail.com>
2016-12-11 06:18:21 +0000

    Arrange the code

--- scripts/wall_stop.py
+++ scripts/wall_stop.py
@@ -8,25 +8,23 @@ from pimouse_ros.msg import LightSensorValues
 class WallTrace():
     def __init__(self):
         self.cmd_vel = rospy.Publisher('/cmd_vel',Twist,queue_size=1)
-
-        self.sensor_values = LightSensorValues()
         rospy.Subscriber('/lightsensors', LightSensorValues, self.callback_lightsensors)
+        self.sensor_values = LightSensorValues()
 
     def callback_lightsensors(self,messages):
         self.sensor_values = messages
 
+    def decision(self,sensor_sum_all):
+        linear = 0.0 if sensor_sum_all >= 500 else 0.2
+        angular = 0.0
+        return linear, angular
+
     def run(self):
         rate = rospy.Rate(10)
-        data = Twist()
-
+        d = Twist()
         while not rospy.is_shutdown():
-            data.linear.x = 0.2
-            data.angular.z = 0
-            if self.sensor_values.sum_all >= 500:
-                data.linear.x = 0
-                data.angular.z = 0
-
-            self.cmd_vel.publish(data)
+            d.linear.x, d.angular.z = self.decision(self.sensor_values.sum_all)
+            self.cmd_vel.publish(d)
             rate.sleep()
 
 if __name__ == '__main__':
