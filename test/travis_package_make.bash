fab1f36c748ff85cff9109d1c68418c4761616c6
Ryuichi Ueda <ryuichiueda@gmail.com>
2016-12-11 02:18:35 +0000

    Add depth for reduce log

--- test/travis_package_make.bash
+++ test/travis_package_make.bash
@@ -5,7 +5,7 @@ rsync -av ./ ~/catkin_ws/src/pimouse_run_corridor/
 
 #clone pimouse_ros
 cd ~/catkin_ws/src/
-git clone https://github.com/citueda/pimouse_ros.git
+git clone --depth=1 https://github.com/citueda/pimouse_ros.git
 
 cd ~/catkin_ws
 catkin_make
